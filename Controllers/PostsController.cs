4b085fd7962273ecb27200a6925225ac9030473f
Shehroz Amjad <shehrozaaa@gmail.com>
2024-05-18 17:14:47 +0500

    Implementing a GET operation

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -1,4 +1,5 @@
 using basic_rest_api.Models;
+using basic_rest_api.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace basic_rest_api.Controllers;
@@ -7,14 +8,24 @@ namespace basic_rest_api.Controllers;
 [ApiController]
 public class PostsController : ControllerBase
 {
-    [HttpGet]
-    public ActionResult<IEnumerable<Post>> GetPost()
+    private readonly PostService _postService;
+    public PostsController(PostService postService)
     {
-        return new List<Post>
+        // Note that we use the new() constructor to create an instance of the service. 
+        // That means the controller is coupled with the PostsService class. 
+        // We will see how to decouple the controller and the service in the DI concept.
+        _postService = new PostService();
+    }
+    //  [HttpGet("{id}")] attribute to indicate the URL of the operation. 
+    //  The URL will be mapped to /api/posts/{id}.
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Post>> GetPost(int id)
+    {
+        var post = await _postService.GetPost(id);
+        if(post == null)
         {
-            new() { Id = 1, UserId = 1, Title = "Post1", Body = "The first post." },
-            new() { Id = 2, UserId = 1, Title = "Post2", Body = "The second post." },
-            new() { Id = 3, UserId = 1, Title = "Post3", Body = "The third post." }
-        };
+            return NotFound();
+        }
+        return Ok(post);
     }
 }
